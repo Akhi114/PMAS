0601a44cbbb49dac75063444552b3d840a04da0c
Akhi114 <akhilla76@gmail.com>
2025-04-08 15:16:24 +0530

    first commit

---
+++ src/main/java/com/Springboot/PMAS/PmasApplication.java
@@ -0,0 +1,13 @@
+package com.Springboot.PMAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmasApplication.class, args);
+	}
+
+}
